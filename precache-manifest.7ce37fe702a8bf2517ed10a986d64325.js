4ad3776c385849514c2698293bd4450952dc7a12
lxdesign-ie <alex_mcgovernsmith@icloud.com>
2020-09-28 18:08:11 +0100

    Updates

---
+++ precache-manifest.7ce37fe702a8bf2517ed10a986d64325.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c049d9982220615172916b37a4b36735",
+    "url": "/react-stopwatch/index.html"
+  },
+  {
+    "revision": "657519559e40a59b59f5",
+    "url": "/react-stopwatch/static/css/main.58b68806.chunk.css"
+  },
+  {
+    "revision": "136425ad32691b6952d9",
+    "url": "/react-stopwatch/static/js/2.7f7b7fc3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-stopwatch/static/js/2.7f7b7fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657519559e40a59b59f5",
+    "url": "/react-stopwatch/static/js/main.1fc39810.chunk.js"
+  },
+  {
+    "revision": "73bf0c21e233a512bbca",
+    "url": "/react-stopwatch/static/js/runtime-main.821ccdb1.js"
+  },
+  {
+    "revision": "0af2a4d05eb5acfba558934fd404ba18",
+    "url": "/react-stopwatch/static/media/face.0af2a4d0.svg"
+  },
+  {
+    "revision": "b7f9532429e13aff2497d2b750185c7f",
+    "url": "/react-stopwatch/static/media/hand.b7f95324.svg"
+  },
+  {
+    "revision": "e6336a031cf95a8135855c4e16d827b4",
+    "url": "/react-stopwatch/static/media/hand_red.e6336a03.svg"
+  }
+]);
